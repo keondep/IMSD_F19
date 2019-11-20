0d629e9d834e35e76d0455171f2fd6cc8105bb71
keon <kdgamer893@gmail.com>
2019-11-20 00:24:03 -0500

    functions

---
+++ functions/Assets/scalefunction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scalefunction : MonoBehaviour
+{
+    public float increasescale = 1f;
+    public float decreasescale = 6f;
+    Transform[] box = new Transform[3];
+    public GameObject prefabcube;
+
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+ GameObject g = Instantiate(prefabcube, new Vector3(-8 + 2 * i, 0, 0), Quaternion.identity);
+        box[i] = g.transform;
+        }
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleup(box[0], Input.GetKey(KeyCode.U));
+    }
+    void scaleup(Transform cubes, bool scaledown)
+    {
+        if (scaledown)
+            cubes.localScale = new Vector3(Time.deltaTime * increasescale, Time.deltaTime * increasescale, Time.deltaTime * increasescale);
+        else
+            cubes.localScale = new Vector3(Time.deltaTime - increasescale, Time.deltaTime - increasescale, Time.deltaTime - increasescale);
+    }
+}
