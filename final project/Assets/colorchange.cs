48285757fc273f0da3c9f74ef5b566c4d8464135
keon <kdgamer893@gmail.com>
2019-12-01 14:26:02 -0500

    final proj

---
+++ final project/Assets/colorchange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class colorchange : MonoBehaviour
+{
+    public Color startColor;
+    public Color mouseOverColor;
+    bool mouseOver = false;
+    public GameObject pic;
+    void OnMouseEnter()
+    {
+        mouseOver = true;
+        GetComponent<Renderer>().material.SetColor("_Color", mouseOverColor);
+        pic.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        mouseOver = false;
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+        pic.SetActive(false);
+    }
+}
