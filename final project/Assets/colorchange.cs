993bd22b21c8b76e9ace31dbb8e3f3542b655cbf
QuentonBlake <qblake139@gmail.com>
2019-12-08 16:43:26 -0500

    Final 3
    Added Pictures and tried to do timer

--- final project/Assets/colorchange.cs
+++ final project/Assets/colorchange.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 public class colorchange : MonoBehaviour
 {
+    private float setTime;
+    private float someAmountOfTime;
     public Color startColor;
     public Color mouseOverColor;
     bool mouseOver = false;
     public GameObject pic;
+
+    private void Start()
+    {
+        setTime = 0;
+        someAmountOfTime = 5;
+    }
     void OnMouseEnter()
     {
         mouseOver = true;
         GetComponent<Renderer>().material.SetColor("_Color", mouseOverColor);
+        //  if (setTime < someAmountOfTime)
+        // {
+        //   pic.SetActive(true);
+        // }
         pic.SetActive(true);
+
     }
     void OnMouseExit()
     {
         mouseOver = false;
         GetComponent<Renderer>().material.SetColor("_Color", startColor);
+       // if (setTime > someAmountOfTime)
+       // {
+        //  pic.SetActive(false);
+        // setTime = 0;
+       // }
         pic.SetActive(false);
     }
 }
